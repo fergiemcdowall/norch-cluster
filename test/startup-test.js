537a81db6962de5d863f15678507a35d1cfed2ca
Fergie McDowall <fergusmcdowall@gmail.com>
2015-10-04 19:41:01 +0200

    newest version of norch

--- test/startup-test.js
+++ test/startup-test.js
@@ -97,13 +97,11 @@ describe('Am I A Happy Norch-Cluster?', function() {
     });
   });
 
-
   it('should do a search', function(done) {
     var q = {};
     q.query = {'*': ['reuter', '1987']};
     q.pageSize = 5;
     nc.search(q, function(err, result) {
-//      console.log(result);
       result.query.should.eql(q);
       result.hits.length.should.be.exactly(5);
       result.totalHits.should.be.exactly(10);
